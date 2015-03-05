6f7a404c437c19dc2994701f48db42fb0527c736
Sabrina Li <sabrinali93@gmail.com>
2015-03-05 02:09:30 -0500

    moved category list to the top and added tags in the footer

--- minimalsimple/content-single.php
+++ minimalsimple/content-single.php
@@ -6,6 +6,14 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+	<?php
+		/* translators: used between list items, there is a space after the comma */
+		$category_list = get_the_category_list( __(',','minimalsimple'));
+
+		if ( minimalsimple_categorized_blog()){
+			echo'<div class="category-list">' . $category_list . '</div>';
+		}
+	?>
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 		<div class="entry-meta">
@@ -24,6 +32,9 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
+	<?php 
+		echo get_the_tag_list ('<ul><li><i class="fa fa-tag"></i>','</li><li><i class="fa fa-tag"></i>','</li></ul>');
+	?>
 		<?php minimalsimple_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
