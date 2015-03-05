01071c7c63a847d14c1b5d15f1e7ff246fb80960
Sabrina Li <sabrinali93@gmail.com>
2015-03-05 00:03:43 -0500

    Something didn't work for the header, removed to try again

--- minimalsimple/header.php
+++ minimalsimple/header.php
@@ -22,14 +22,14 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'minimalsimple' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-			<?php if ( get_header_image() && ('blank' == get_header_textcolor()) ) : ?>
+			<?php if ( get_header_image() ) : ?>
 				<div class="header-image">
 					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
 							<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
 					</a>
 				</div>	
 			<?php endif; // End header image check. ?>
-		<div class="site-branding" style ='background-image: url (<?php header_image(); ?>)'>
+		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div><!-- .site-branding -->
