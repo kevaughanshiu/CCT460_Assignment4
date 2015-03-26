92adf786225a80080d3d4a49fb06de57005a3731
Sabrina Li <sabrinali93@gmail.com>
2015-03-26 15:04:20 -0400

    added comments and provided second icon images for each social media

--- Widget_Assignment/socialmediahub.php
+++ Widget_Assignment/socialmediahub.php
@@ -25,7 +25,7 @@ add_action('wp_enqueue_scripts','register_stylesheets',99); //added 99 to it bec
 
 class socialmediahub extends WP_Widget
 {
-
+  //provided description of what the plugin/widget is
 	function socialmediahub()
 	{
 		$widget_option = array(
@@ -33,18 +33,20 @@ class socialmediahub extends WP_Widget
 			'description' => 'Social media links for user');
 		parent::WP_Widget('socialmediahub', 'Social Media Hub', $widget_option);
 	}
-
+  //Conditions to print. Allows only the user to type their usernames instead of the entire "http://twitter.com/[username]"etc.
 	function widget($args, $instance)
 	{
-		extract($args, EXTR_SKIP);
+		//tells the computer if something is in the textbox then use what was inputted in the textbox and apply it at the end of the website or *special case: tumblr is inbetween*
+    extract($args, EXTR_SKIP);
 		$title = 'Follow Me Here';
 		$twitter=($instance['twitter']) ? 'http://twitter.com/' . $instance['twitter'] : 'N/A';
     $instagram=($instance['instagram']) ? 'http://www.instagram.com/' . $instance['instagram'] : 'N/A';
     $facebook=($instance['facebook']) ? 'http://www.facebook.com/' . $instance['facebook'] : 'N/A';
     $google=($instance['google']) ? 'http://www.google.com/+' . $instance['google'] : 'N/A';
     $tumblr=($instance['tumblr']) ? 'http://' . $instance['tumblr'] . '.tumblr.com' : 'N/A';
+    //was not able to figure out how to make it not display at all if there was nothing inputted in the textbox
 ?>
-
+ <!--what shows up on the fron of the website, it shows the icon and makes it a clickable icon to direct them to the social media pages --> 
 	<div class ="frontface">
 		<?php echo $before_widget; ?>
 		<?php echo $before_title . $title . $after_title ?>
@@ -63,17 +65,20 @@ class socialmediahub extends WP_Widget
 	{
 
 ?>
+<!--backhouse form for the user to put their social media links in-->
 	<label for="<?php echo $this->get_field_id('twitter');?>">
                Twitter Username: 
 
                     <input id="<?php echo $this->get_field_id('twitter');?>"
                     name="<?php echo $this->get_field_name('twitter'); ?>"
                     value="<?php echo esc_attr($instance['twitter']);?>" /> 
+                <!--used radio buttons to allow user to choose a specific icon out of the two, was not able to figure out how to add boolean if/else if function-->    
                  <p>
                     <label for="radio1">Choose:</label>
                     <input name="radio1" type="radio" value="twitter1" checked="checked" id="twitter1"/>
+                        <img src="http://i.imgur.com/FfOQSXU.png" style="width:30px; height:30px">
                     <input name="radio1" type="radio" value="twitter2" id="twitter2"/>
-                    <img src="http://i.imgur.com/Mv8pCdy.png" style="width:30x; height:30px">
+                        <img src="http://i.imgur.com/Mv8pCdy.png" style="width:30x; height:30px">
                  </p>   
           </label> 
           <br/>
@@ -85,8 +90,9 @@ class socialmediahub extends WP_Widget
                    <p>
                     <label for="radio2">Choose:</label>
                     <input name="radio2" type="radio" value="instagram1" checked="checked" id="instagram1"/>
-                    <input name="radio2" type="radio" value="instagram2" id="instagram"/>
-                    <img src="http://i.imgur.com/VGWFXae.png" style="width:30x; height:30px">
+                        <img src="http://i.imgur.com/oJ1p1BR.png" style="width:30px; height:30px">
+                    <input name="radio2" type="radio" value="instagram2" id="instagram2"/>
+                        <img src="http://i.imgur.com/VGWFXae.png" style="width:30x; height:30px">
                  </p>   
           </label>
           <br/>
@@ -96,10 +102,11 @@ class socialmediahub extends WP_Widget
                     name="<?php echo $this->get_field_name('facebook'); ?>"
                     value="<?php echo esc_attr($instance['facebook']);?>" />
                     <p>
-                    <label for="radio2">Choose:</label>
-                    <input name="radio2" type="radio" value="instagram1" checked="checked" id="instagram1"/>
-                    <input name="radio2" type="radio" value="instagram2" id="instagram"/>
-                    <img src="http://i.imgur.com/jeEPz6J.png" style="width:30x; height:30px">
+                    <label for="radio3">Choose:</label>
+                    <input name="radio3" type="radio" value="facebook1" checked="checked" id="facebook1"/>
+                        <img src="http://i.imgur.com/BxfLpwK.png" style="width:30px; height:30px">
+                    <input name="radio3" type="radio" value="facebook2" id="facebook2"/>
+                        <img src="http://i.imgur.com/jeEPz6J.png" style="width:30x; height:30px">
                  </p> 
           </label>
           <br/>
@@ -109,9 +116,10 @@ class socialmediahub extends WP_Widget
                     name="<?php echo $this->get_field_name('google'); ?>"
                     value="<?php echo esc_attr($instance['google']);?>" />
                     <p>
-                    <label for="radio2">Choose:</label>
-                    <input name="radio2" type="radio" value="instagram1" checked="checked" id="instagram1"/>
-                    <input name="radio2" type="radio" value="instagram2" id="instagram"/>
+                    <label for="radio4">Choose:</label>
+                    <input name="radio4" type="radio" value="google1" checked="checked" id="google1"/>
+                      <img src="http://i.imgur.com/hZM0U57.png" style="width:30px; height:30px">
+                    <input name="radio4" type="radio" value="google2" id="google2"/>
                     <img src="http://i.imgur.com/Fd7pXPC.png" style="width:30x; height:30px">
                  </p> 
           </label>
@@ -122,9 +130,10 @@ class socialmediahub extends WP_Widget
                     name="<?php echo $this->get_field_name('tumblr'); ?>"
                     value="<?php echo esc_attr($instance['tumblr']);?>" />
                     <p>
-                    <label for="radio2">Choose:</label>
-                    <input name="radio2" type="radio" value="instagram1" checked="checked" id="instagram1"/>
-                    <input name="radio2" type="radio" value="instagram2" id="instagram"/>
+                    <label for="radio5">Choose:</label>
+                    <input name="radio5" type="radio" value="tumblr1" checked="checked" id="tumblr1"/>
+                      <img src="http://i.imgur.com/et6kEzp.png" style="width:30px; height:30px">
+                    <input name="radio5" type="radio" value="tumblr2" id="tumblr2"/>
                     <img src="http://i.imgur.com/HDHpL0K.png" style="width:30x; height:30px">
                  </p> 
           </label>
