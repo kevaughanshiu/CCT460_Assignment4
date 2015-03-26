193d407f8af832148302ed1cafa3148c4f256464
JosephDWM <joe.measures@gmail.com>
2015-03-26 15:36:37 -0400

    shortcode fix?

--- minimalsimple/my_shortcodes.php
+++ minimalsimple/my_shortcodes.php
@@ -1,7 +1,23 @@
-<?php 
-function kase_short(){
+<?php
+/*
+Plugin Name: Kase Shortcode
+Description: This plugin is used to show your social media buttons on your posts
+Version: 1.0
+Author: Sabrina Li, Joe Measures, Kevaughan Shiu
+*/
+
+
+function kase_short($atts, $content = null){
+	extract(shortcode_atts(array(
+		'class' => ''
+	), $atts));
 	
-echo 'Thanks for following';
+	return '<div class="kasesc">' . do_shortcode($content) . '</div>';
 }
+function kase_short_register() {
+	add_shortcode('kase_short_sc', 'kase_short');
+}
+
+add_action('init', 'kase_short_register');
 
 ?>
